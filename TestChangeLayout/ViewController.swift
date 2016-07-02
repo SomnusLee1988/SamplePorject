7afd43e69d3f13c4eb6d9e6838d6aa6f73585fb3
SomnusLee <somnuslee1988@gmail.com>
2016-07-02 15:50:48 +0800

    Revert "using autolayout to switch position of views"

--- TestChangeLayout/ViewController.swift
+++ TestChangeLayout/ViewController.swift
@@ -9,106 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let firstView = UIView()
-    let secondView = UIView()
 
+    @IBOutlet var view1: UIView!
+    @IBOutlet var view2: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.firstView.backgroundColor = UIColor(red: 0.75, green: 0.75, blue: 0.1, alpha: 1.0)
-        self.secondView.backgroundColor = UIColor.redColor()
-
-        self.createAutoLayoutForViews(firstView, view2: secondView)
+        print("view1.frame = \(self.view1.frame)")
+        print("view2.frame = \(self.view2.frame)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func createAutoLayoutForViews(view1: UIView, view2: UIView) {
-        view1.translatesAutoresizingMaskIntoConstraints = false
-        view2.translatesAutoresizingMaskIntoConstraints = false
-        
-        //Add the views
-        view.addSubview(view1)
-        view.addSubview(view2)
-        
-        let viewsDictionary = [
-            "view1":view1,
-            "view2":view2 ]
-        
-        let metricsDictionary = [
-            "view1Height": 50.0,
-            "view2Height":40.0,
-            "viewWidth":50.0 ]
-        
-        self.addViewOneConstraints(view1, viewsDictionary: viewsDictionary, metricsDictionary: metricsDictionary)
-        self.addViewTwoConstraints(view2, viewsDictionary: viewsDictionary, metricsDictionary: metricsDictionary)
-        self.addSuperViewConstraints(viewsDictionary, metricsDictionary: metricsDictionary)
-    }
-    
-    func addViewOneConstraints(view1: UIView, viewsDictionary: [String:UIView], metricsDictionary: [String:Double]) {
-        let view1_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[view1(viewWidth)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        let view1_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[view1(view1Height)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        view1.addConstraints(view1_constraint_H)
-        view1.addConstraints(view1_constraint_V)
-    }
-
-    
-    func addViewTwoConstraints(view2: UIView, viewsDictionary: [String:UIView], metricsDictionary: [String:Double]) {
-        let view2_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[view2(viewWidth)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        let view2_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[view2(>=view2Height)]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: metricsDictionary,
-            views: viewsDictionary)
-        
-        view2.addConstraints(view2_constraint_H)
-        view2.addConstraints(view2_constraint_V)
-    }
-    
-    func addSuperViewConstraints(viewsDictionary: [String:UIView], metricsDictionary: [String:Double]) {
-        let view_constraint_H = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-[view2]",
-            options: NSLayoutFormatOptions(rawValue:0),
-            metrics: nil, views: viewsDictionary)
-        let view_constraint_V = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-36-[view1]-8-[view2]-200-|",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil, views: viewsDictionary)
-        
-        view.addConstraints(view_constraint_H)
-        view.addConstraints(view_constraint_V)
-    }
 
     @IBAction func change(sender: AnyObject) {
-        // Remove existing constraints
-        self.firstView.removeConstraints(self.firstView.constraints)
-        self.secondView.removeConstraints(self.secondView.constraints)
-        self.firstView.removeFromSuperview()
-        self.secondView.removeFromSuperview()
         
-        // Decide which view will recieve which constraints now based on their current sizes
-        if firstView.frame.height == 50 {
-            self.createAutoLayoutForViews(self.secondView, view2: self.firstView)
-        } else {
-            self.createAutoLayoutForViews(self.firstView, view2: self.secondView)
-        }
+        let frame = view1.frame
+        view1.frame = view2.frame
+        view2.frame = frame
     }
+
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        
+    }
+
+
 }
 
